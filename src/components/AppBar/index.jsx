828be7da5bc2055770e8194f5688e082e744f17b
DungNguyen <nguyenvandung12112002@gmail.com>
2024-05-06 00:02:09 +0700

    Config prettier

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,15 +1,18 @@
 import { Box } from '@mui/material'
-import ModeSelect from '~/component/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
 
 const AppBar = () => {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.light',
-      width: '100%',
-      height: (theme) => theme.trello.appBarHeight,
-      display: 'flex',
-      alignItems: 'center'
-    }}>
+    <Box
+      sx={{
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Box>Trello</Box>
       <ModeSelect />
     </Box>
   )
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -2,15 +2,15 @@ import { Box } from '@mui/material'
 
 const BoardBar = () => {
   return (
-    <Box sx={{
-      backgroundColor: 'primary.dark',
-      width: '100%',
-      height: (theme) => theme.trello.boardBarHeight,
-      display: 'flex',
-      alignItems: 'center'
-
-    }}>
-        Board bar
+    <Box
+      sx={{
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      Board bar
     </Box>
   )
 }
