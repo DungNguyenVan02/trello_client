463e2690a1873c505bca3866d2a1e857f50eeaf0
DungNguyen <nguyenvandung12112002@gmail.com>
2024-05-05 15:36:41 +0700

    Dark_light_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
-import Button from '@mui/material/Button'
-import { Typography } from '@mui/material'
+import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 import { useColorScheme } from '@mui/material/styles'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function ModeToggle() {
+
+function SelectTheme() {
   const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="label-select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light" >
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark" >
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system" >
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 function App() {
   return (
     <>
-      <ModeToggle />
+      <SelectTheme />
       <Typography variant='body1' color='text.secondary'>
         Hello dung nguyen
       </Typography>
-      <Button variant="contained">Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
     </>
   )
 }
